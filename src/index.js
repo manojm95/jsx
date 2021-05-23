8b938bfa9d58c5a62ef1f72eff4efacf765ce475
Manoj Raju <manojm95@gmail.com>
2021-05-23 10:49:27 -0400

    changed

--- src/index.js
+++ src/index.js
@@ -1,16 +1,31 @@
 // importing react and react dom libs
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 //create a react component
 //funtion based component 
-const App = function() {
-    return <div> hello there</div>
-}
+let temp={fontSize:30}
+// const App = function() {//instead of function() use = ()=>
+// console.lg("Text"); 
+//     return (
+//         <div>
+//     <label style={temp}>Enter Name:</label>
+//     <input id = "name" type="text" value="NNNN" ></input>
+//     <button style = {{backgroundColor : 'red', color: 'white'}} > Submit </button>
+
+//     </div>
+//     );
+// };
+
+const App = ()=> <div>
+<label style={temp}>Enter Name:</label>
+<input id = "name" type="text" value="NNNN" ></input>
+<button style = {{backgroundColor : 'red', color: 'white'}} > Submit </button>
+
+</div>
 
 //put the react component on the screen
 //query selector is a built in function
 ReactDOM.render(
     <App />,
-    document.querySelector("#root")
-);
+    document.querySelector('#root'));
